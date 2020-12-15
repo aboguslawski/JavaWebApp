b041fe723265dfd4c7d8ee9a1f25c03b1beccdf6
Adam Boguslawski <adam.boguslawski1998@gmail.com>
2020-12-15 23:39:41 +0100

    reading from csv

--- src/main/java/com/aboguslawski/blog/model/PostData.java
+++ src/main/java/com/aboguslawski/blog/model/PostData.java
@@ -1,8 +1,10 @@
 package com.aboguslawski.blog.model;
 
+import com.aboguslawski.blog.util.OpenCSVReader;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -16,12 +18,15 @@ public class PostData {
 
     // == constructors ==
     public PostData() {
-        addPost(new Post(1, "Hilliary Negal", "Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat."));
-        addPost(new Post(2, "Saleem Voaden", "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat."));
-        addPost(new Post(3, "Stanford Mennithorp", "Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem."));
-        addPost(new Post(4, "Celia Legier", "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros."));
-        addPost(new Post(5, "Ilyssa Bowater", "Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam."));
-
+        try{
+            OpenCSVReader.postsData(posts);
+        }catch (IOException e){
+            addPost(new Post(1, "Hilliary Negal", "Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat."));
+            addPost(new Post(2, "Saleem Voaden", "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat."));
+            addPost(new Post(3, "Stanford Mennithorp", "Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem."));
+            addPost(new Post(4, "Celia Legier", "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros."));
+            addPost(new Post(5, "Ilyssa Bowater", "Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam."));
+        }
     }
 
     // == public methods ==
---
+++ src/main/java/com/aboguslawski/blog/util/OpenCSVReader.java
@@ -0,0 +1,35 @@
+package com.aboguslawski.blog.util;
+
+import com.aboguslawski.blog.model.Post;
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class OpenCSVReader {
+
+    private static final String POSTS_PATH = "C:\\Users\\Adam Bogusławski\\Desktop\\Programowanie\\Java\\blog\\src\\main\\resources\\OnePosts.csv";
+
+    public static void postsData(List<Post> sourceList) throws IOException {
+        int postId;
+        String postUser;
+        String postContent;
+
+        Reader reader = Files.newBufferedReader(Paths.get(POSTS_PATH));
+        CSVReader csvReader = new CSVReader(reader);
+        String[] nextRecord;
+        csvReader.readNext();
+
+        while ((nextRecord = csvReader.readNext()) != null) {
+            postId = Integer.parseInt(nextRecord[0]);
+            postUser = nextRecord[1];
+            postContent = nextRecord[2];
+
+            sourceList.add(new Post(postId, postUser, postContent));
+        }
+    }
+
+}
