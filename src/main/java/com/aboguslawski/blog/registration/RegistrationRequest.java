205677ac56155bd811665fb9efb705bd97e6dfcd
Adam Boguslawski <adam.boguslawski1998@gmail.com>
2021-01-19 17:35:52 +0100

    user registration

---
+++ src/main/java/com/aboguslawski/blog/registration/RegistrationRequest.java
@@ -0,0 +1,16 @@
+package com.aboguslawski.blog.registration;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+
+    private final String firstName;
+    private final String lastName;
+    private final String password;
+    private final String email;
+
+}
