e65dcc918043792f3369e1624ff6b5210040c3f0
Adam Boguslawski <adam.boguslawski1998@gmail.com>
2021-01-19 18:54:37 +0100

    further fixing

--- src/main/java/com/aboguslawski/blog/registration/RegistrationController.java
+++ src/main/java/com/aboguslawski/blog/registration/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.aboguslawski.blog.registration;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ public class RegistrationController {
 
     private RegistrationService registrationService;
 
+    // test
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
