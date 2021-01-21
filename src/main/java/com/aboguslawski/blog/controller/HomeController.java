9bda7df0f6ea6bd053e12e8aa297c5c68091f13a
Adam Boguslawski <adam.boguslawski1998@gmail.com>
2021-01-21 22:27:38 +0100

    header fragment

--- src/main/java/com/aboguslawski/blog/controller/HomeController.java
+++ src/main/java/com/aboguslawski/blog/controller/HomeController.java
@@ -1,8 +1,6 @@
 package com.aboguslawski.blog.controller;
 
-import com.aboguslawski.blog.model.post.Post;
 import com.aboguslawski.blog.model.post.PostService;
-import com.aboguslawski.blog.model.user.User;
 import com.aboguslawski.blog.model.user.UserService;
 import com.aboguslawski.blog.util.Mappings;
 import com.aboguslawski.blog.util.ViewNames;
@@ -10,7 +8,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
@@ -19,16 +16,19 @@ public class HomeController {
     private final PostService postService;
     private final UserService userService;
 
-    @GetMapping("currentUser")
-    public String currentUserName(){
-        return userService.currentUserName();
-    }
-
     @GetMapping(Mappings.HOME)
-    public String home(Model model){
+    public String home(Model model) {
         model.addAttribute("postService", postService);
         model.addAttribute("userService", userService);
         return ViewNames.HOME;
+    }
+
+    @GetMapping(Mappings.NEW_POST)
+    public String newPost(Model model) {
+        model.addAttribute("postService", postService);
+        model.addAttribute("userService", userService);
+
+        return ViewNames.NEW_POST;
     }
 
 
--- src/main/java/com/aboguslawski/blog/util/ViewNames.java
+++ src/main/java/com/aboguslawski/blog/util/ViewNames.java
@@ -4,7 +4,7 @@ public final class ViewNames {
 
     // == constants ==
     public static final String HOME = "home";
-    public static final String ADD_POST = "add_post";
+    public static final String NEW_POST = "new_post";
     public static final String VIEW_POST = "view_post";
     public static final String EDIT_POST = "edit_post";
 
